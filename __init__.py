b9b69e4bb978ad22cb269cbbf01dd6d673ca70af
brian <406096277@qq.com>
2021-01-27 10:17:14 +0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# _*_coding:utf-8_*_
+# Name:Brian
+# Create_time:2021/1/27 10:04
+# file: __init__.py.py
+# location:chengdu
+# number:610000
---
+++ data_load.py
@@ -0,0 +1,14 @@
+# _*_coding:utf-8_*_
+# Name:Brian
+# Create_time:2021/1/21 13:59
+# file: data_load.py
+# location:chengdu
+# number:610000
+import pandas as pd
+import  numpy as np
+import time
+import copy
+import torch
+from torch.autograd import Variable
+import torch
+import matplotlib.pylab as plt
